24d0eecccc342e1cefb8b46c53db76efbb950610
Jurgen Lange Bregado <56806395+jurgenlange1967@users.noreply.github.com>
2025-02-24 00:20:25 -0300

    Update main.py

--- Examples/Example-4/main.py
+++ Examples/Example-4/main.py
@@ -1,1 +1,8 @@
+from bsw import BSW
 
+four_simul = BSW()
+four_simul.load_data('all-5-oil-with-api.csv', kalman_filtered=False)
+four_simul.fit(with_restriction=False)
+four_simul.Predict()
+four_simul.sensitivity_analysis_on_input_variables(exp_nsample=12)
+four_simul.sensitivity_analysis_on_parameters()
