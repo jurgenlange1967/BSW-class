46a967e956718e59f8656dbb26afaa071e57cae1
Jurgen Lange Bregado <56806395+jurgenlange1967@users.noreply.github.com>
2025-02-24 00:18:33 -0300

    Update main.py

--- Examples/Example-1/main.py
+++ Examples/Example-1/main.py
@@ -1,1 +1,10 @@
+from bsw import BSW
 
+first_simul = BSW()
+first_simul.load_data('p-66-otra-forma-input-2-without-atypical-values-with-all-values.csv')
+# In the case of bad fitting by the model - use then with_restriction=False or modify the initial guesses
+first_simul.fit(with_restriction=False)
+first_simul.Predict()
+first_simul.plot_correlation_matrix_with_heatmap()
+first_simul.show_data_filtered()
+first_simul.show_only_experimental_data()
