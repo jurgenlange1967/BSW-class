cee7f72fe94e20368cc22879161a69ab74048dae
Jurgen Lange Bregado <56806395+jurgenlange1967@users.noreply.github.com>
2025-02-25 23:17:53 -0300

    Update main.py

--- Examples/Example-1/main.py
+++ Examples/Example-1/main.py
@@ -3,15 +3,15 @@ from bsw import BSW
 # Instantiate a object type BSW
 first_simul = BSW()
 # Load um arquivo .csv and uses a Kalman filter for this data (see docs/documentation)
-# Note that this method does not return nothing and the cut value is by default = 0
-# indicating that ther is no test data, thereby the train data is equal to test data
+# Note that this method does not return nothing because the cut value is by default = 0
+# indicating that there is no test data, thereby the train data is equal to test data
 first_simul.load_data('p-66-otra-forma-input-2-without-atypical-values-with-all-values.csv')
-# fit the model to the data loaded. The optimization of parameters in the model is performed
+# fit the model to the loaded data. The optimization of parameters in the model is performed
 # without using restriction (see docs/documentation)
 first_simul.fit(with_restriction=False)
 # The method predict() shows the simulated and experimental data together confidence bands and 
 # mean prediction (see docs/documentation).
-first_simul.Predict()
+first_simul.predict()
 # Shows a correlation matrix between parameters (see docs/documentation)
 first_simul.plot_correlation_matrix_with_heatmap()
 # The method shows the all filtered data using the Kalman filter (see docs/documentation)
