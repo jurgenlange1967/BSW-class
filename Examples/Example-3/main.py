fbaee5b68e3d77dfb416a90c991cabcb826d5f0e
Jurgen Lange Bregado <56806395+jurgenlange1967@users.noreply.github.com>
2025-02-25 23:16:25 -0300

    Update main.py

--- Examples/Example-3/main.py
+++ Examples/Example-3/main.py
@@ -1,7 +1,12 @@
 from bsw import BSW
 
+# Instantiate a object type BSW
 third_simul = BSW()
+# Load um arquivo .csv and not uses the Kalman filter for this data (see docs/documentation)
 third_simul.load_data('all-5-oil.csv', kalman_filtered=False)
+# Fit the model to the loaded data. The optimization of parameters in the model is performed
+# with constraints (see docs/documentation)
 third_simul.fit()
-third_simul.Predict(showErr=False)
-
+# The method predict() uses all load data to make prediction (test set = training set)   
+# This method does not shows confidence bands and mean prediction (see docs/documentation).
+third_simul.predict(showErr=False)
